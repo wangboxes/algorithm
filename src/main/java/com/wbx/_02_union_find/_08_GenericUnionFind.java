d81f977d314a398ebf21e6358e7f1f762b66be98
wangboxes <1024720098@qq.com>
2019-10-21 10:50:37 +0800

    并查集--自定义对象实现

---
+++ src/main/java/com/wbx/_02_union_find/_08_GenericUnionFind.java
@@ -0,0 +1,83 @@
+package com.wbx._02_union_find;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @describe： 自定义对象的并查集, Quick Union + 基于rank的优化 + Path Halving
+ * @Date：2019/10/21 10:19
+ * @author：wbx
+ */
+public class _08_GenericUnionFind<V> {
+
+
+    private Map<V, Node<V>> nodes = new HashMap<>();
+
+    public void makeSet(V v) {
+        if (!nodes.containsKey(v)) {
+            nodes.put(v, new Node<>(v));
+        }
+    }
+
+
+    public V find(V v) {
+        Node<V> node = findNode(v);
+        return node == null ? null : node.value;
+    }
+
+    /**
+     * 找出v的根节点
+     */
+    private Node<V> findNode(V v) {
+        Node<V> node = nodes.get(v);
+
+        if (node == null) {
+            return null;
+        }
+
+        while (!Objects.equals(node.value, node.parent.value)) {
+            node.parent = node.parent.parent;
+            node = node.parent;
+        }
+
+        return node;
+    }
+
+    public void union(V v1, V v2) {
+        Node<V> p1 = findNode(v1);
+        Node<V> p2 = findNode(v2);
+        if (p1 == null || p2 == null) {
+            return;
+        }
+        if (Objects.equals(p1.value, p2.value)) {
+            return;
+        }
+
+        if (p1.rank < p2.rank) {
+            p1.parent = p2;
+        } else if (p1.rank > p2.rank) {
+            p2.parent = p1;
+        } else {
+            p1.parent = p2;
+            p2.rank += 1;
+        }
+    }
+
+    public boolean isSame(V v1, V v2) {
+        return Objects.equals(find(v1), find(v2));
+    }
+
+    private static class Node<V> {
+
+        V value;
+
+        Node<V> parent = this;
+
+        int rank = 1;
+
+        Node(V value) {
+            this.value = value;
+        }
+    }
+}
--- src/test/java/com/wbx/_02_union_find/UnionFindTest.java
+++ src/test/java/com/wbx/_02_union_find/UnionFindTest.java
@@ -1,5 +1,6 @@
 package com.wbx._02_union_find;
 
+import com.wbx.tools.Asserts;
 import com.wbx.tools.Times;
 import org.junit.Assert;
 import org.junit.Test;
@@ -25,6 +26,43 @@ public class UnionFindTest {
         testTime(new _05_UnionFind_QU_Rank_PC(count));
         testTime(new _06_UnionFind_QU_Rank_PS(count));
         testTime(new _07_UnionFind_QU_Rank_PH(count));
+        testTime(new _08_GenericUnionFind<Integer>());
+    }
+
+    static void testTime(_08_GenericUnionFind<Integer> uf) {
+        for (int i = 0; i < count; i++) {
+            uf.makeSet(i);
+        }
+
+        uf.union(0, 1);
+        uf.union(0, 3);
+        uf.union(0, 4);
+        uf.union(2, 3);
+        uf.union(2, 5);
+
+        uf.union(6, 7);
+
+        uf.union(8, 10);
+        uf.union(9, 10);
+        uf.union(9, 11);
+
+        Asserts.test(!uf.isSame(2, 7));
+
+        uf.union(4, 6);
+
+        Asserts.test(uf.isSame(2, 7));
+
+        Times.test(uf.getClass().getSimpleName(), () -> {
+            for (int i = 0; i < count; i++) {
+                uf.union((int)(Math.random() * count),
+                        (int)(Math.random() * count));
+            }
+
+            for (int i = 0; i < count; i++) {
+                uf.isSame((int)(Math.random() * count),
+                        (int)(Math.random() * count));
+            }
+        });
     }
 
     private void testTime(UnionFind uf) {
